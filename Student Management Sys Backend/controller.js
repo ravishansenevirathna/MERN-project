479e699b56eda92ebc7167adcfd884423838675e
ravishansenevirathna <travishan55@gmail.com>
2024-04-14 23:04:57 +0530

    3rd commit

--- Student Management Sys Backend/controller.js
+++ Student Management Sys Backend/controller.js
@@ -5,15 +5,15 @@ const User = require('./model');
 
 
 
-const getAllUsers = (cb) => {
-    cb(users);
+// const getAllUsers = (cb) => {
+//     cb(users);
 
-};
+// };
 
-const getUserById = (id, cb) => {
-    const user = users.find(user => user.id == id);
-    cb(user);
-};
+// const getUserById = (id, cb) => {
+//     const user = users.find(user => user.id == id);
+//     cb(user);
+// };
 
 exports.getAllUsers = getAllUsers;
 exports.getUserById = getUserById;
